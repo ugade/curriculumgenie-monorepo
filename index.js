aadb047b86668850312277db60bb41171c3615dc
ugade <ugtaqwa@gmail.com>
2025-04-18 18:15:01 +1000

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'react-quill/dist/quill.snow.css';
-ReactDOM.render(<App />, document.getElementById('root'));
